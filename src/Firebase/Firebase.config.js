11dd7b2f14ec635e50bca1dfcc374d8ce62d23c0
asraful613 <atifasraful530@gmail.com>
2024-04-06 11:16:28 +0100

    added a tailwind ,daisyUi and firebase setup

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD3j6h8gch9gs2uMLcW-k8qkYYY_VwktbI",
+  authDomain: "glasses-authentication-f8e42.firebaseapp.com",
+  projectId: "glasses-authentication-f8e42",
+  storageBucket: "glasses-authentication-f8e42.appspot.com",
+  messagingSenderId: "593399334117",
+  appId: "1:593399334117:web:833553e5989af9af81000c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
